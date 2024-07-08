0f58a532db79ea18065adae3e2ac8904bcb8ff67
高梓竣 <gaozijun@qlwlkj.cc>
2024-07-08 10:47:06 +0800

    index finish commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div class=" flex justify-center align-middle gap-32">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="w-32" alt="Vite logo" />
+  <div class=" flex justify-center flex-col items-center border bg-blue-900 h-screen">
+    <div class="title flex text-center flex-col">
+      <div>
+        <span class=" text-white font-bold text-8xl">Asuka</span>
+        <span class="text-white text-7xl">&nbsp;の 脚手架</span>
+      </div>
+      <p class=" mt-6 flex justify-center items-center text-2xl text-white font-extralight">基于<span class=" font-semibold text-green-500">&nbsp;Vue3</span> + <span class=" font-semibold text-orange-400">&nbsp;Vite</span> + <span class=" font-semibold text-blue-500">&nbsp;tailwindcss</span>&nbsp;搭建的快速项目脚手架 🤣 👉 🤡</p>
+    </div>
+
+    <a href="https://github.com/Nikaido-Asuka" target="_blank">
+      <div class="btn mt-10 px-16 py-5 rounded-lg text-center text-white">
+        <div class=" text-lg ">点击开始查看</div>
+        <div class="flex font-light gap-3 justify-center items-center">
+          <img class=" w-5" src="./assets/github.png"/>
+          <span>@Asuka</span>
+        </div>
+      </div>
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="w-32" alt="Vue logo" />
-    </a>
+   
+
+    <div class="power mt-6 h-20 flex items-center">
+      <div class="left  w-1/4 text-lg text-white font-medium">Powered by:</div>
+      <div class="right w-3/4 flex justify-around items-center">
+
+        <a href="https://vitejs.dev" target="_blank">
+          <div class=" flex justify-around items-center gap-2">
+            <img src="./assets/vue.svg" class="w-6" alt="Vue logo" />
+            <span class="text-white">vue.js</span>
+          </div>
+        </a>
+
+        <a href="https://vitejs.dev" target="_blank">
+          <div class=" flex justify-around items-center gap-2">
+            <img src="/vite.svg" class="w-6" alt="Vite logo" />
+            <span class="text-white">vitejs</span>
+          </div>
+        </a>
+
+        <a href="https://vitejs.dev" target="_blank">
+          <div class=" flex justify-around items-center gap-2">
+            <img src="./assets/tailwindCSS.png" class="w-6" alt="Vite logo" />
+            <span class="text-white">tailwindcss</span>
+          </div>
+        </a>
+
+      </div>
+    </div>
+
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+ 
 </template>
 
-<style scoped>
-.vite{
+<script setup lang="ts">
+</script>
 
+
+<style scoped>
+.power{
+  width: 30%;
+}
+.btn{
+  background-color: #0b66c0;
+  cursor: pointer;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,13 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <div class=" mt-9 ">
-    <h1 class=" text-blue-500 text-5xl">{{ msg }}</h1>
-  </div>
- 
-  
+    <div class=" mt-10">
+        
+    </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
 
 </style>
