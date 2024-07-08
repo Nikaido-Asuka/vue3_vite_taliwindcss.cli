7ff53f32244aed546b6b2004c497e3d0b8d95800
高梓竣 <gaozijun@qlwlkj.cc>
2024-07-08 09:55:26 +0800

    seconde commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
-<template>
-  <div class=" ">
-    <p class=" text-purple-600"></p>
-  </div>
-</template>
-
 <script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style scoped>
+<template>
+  <div class=" flex justify-center align-middle gap-32">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="w-32" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="w-32" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
+<style scoped>
+.vite{
+
+}
 </style>
